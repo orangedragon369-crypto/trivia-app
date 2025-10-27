dbfc8c13cec8df2c191af1fef9c95cdfd9f515dc
Benjamin Ludington <orangedragon369@gmail.com>
2025-10-27 01:05:16 -0600

    Add icons to header navigation links

--- triviaGame/script/renderHeader.mjs
+++ triviaGame/script/renderHeader.mjs
@@ -2,8 +2,8 @@ function setHeader(){
     const head = document.getElementById("header");
     head.innerHTML = `
         <div class="cont fill">
-            <div><a class="link" href="index.html">Home</a></div>
-            <div><a class="link" href="stats.html">Stats</a></div>
+            <div><a class="link" href="index.html"><i class="fa-solid fa-house"></i>Home</a></div>
+            <div><a class="link" href="stats.html"><i class="fa-solid fa-chart-simple"></i>Stats</a></div>
         </div>`;
     head.classList.add("header")
 };
